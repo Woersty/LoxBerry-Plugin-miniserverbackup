f717f6138981539f922d76e9e74a2dc42a4f4177
Wörsty <git@loxberry.woerstenfeld.de>
2019-04-03 18:02:34 +0200

    Try firewwall issue protection
    Wait 50 ms between requests to not run into firewall trouble on MS

--- webfrontend/html/createmsbackup.php
+++ webfrontend/html/createmsbackup.php
@@ -539,6 +539,7 @@ foreach ($ms as $msno => $miniserver )
 			}
 			$url = "http://".$miniserver['IPAddress'].":".$miniserver['Port']."/dev/fsget".$file_to_save;
 			debug($L["MINISERVERBACKUP.INF_0016_READ_FROM_WRITE_TO"]."\n".$url ." => ".$workdir_data."/".$bkpfolder.$file_to_save); 
+			usleep(50000);
 			$curl_save = curl_init(str_replace(" ","%20",$url));
 			curl_setopt($curl_save, CURLOPT_USERPWD, $miniserver['Credentials_RAW']);
 			curl_setopt($curl_save, CURLOPT_TIMEOUT, 50);
