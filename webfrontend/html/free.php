9c93ba08817e40993df0ea6c631ce562ec85b66d
Wörsty <git@loxberry.woerstenfeld.de>
2019-04-05 21:17:50 +0200

    Update free.php
    Redesign because still errors in apache log

--- webfrontend/html/free.php
+++ webfrontend/html/free.php
@@ -5,27 +5,8 @@
 error_reporting(0);
 header('Content-Type: text/plain; charset=utf-8');
 $path=file_get_contents("/tmp/msb_free_space");
-$free = "?";
-if ($path != "") 
-{
-	if ( stripos($path, "/system/storage/smb/") )
-	{
-		$free = @exec('df --output=avail '.$path.' 2>/dev/null |grep -v Avail');
-		if ( $free == "" ) $free = @exec('df --output=avail "'.dirname($path).'" 2>/dev/null |grep -v Avail');
-	}
-	else
-	{
-		if ( !is_readable($path) )
-		{
-			if ( is_readable(dirname($path)) )
-			{
-				$free = @exec('df --output=avail '.dirname($path).' 2>/dev/null |grep -v Avail');
-			}
-		}
-		else
-		{
-			$free = @exec('df --output=avail '.$path.' 2>/dev/null |grep -v Avail');
-		}
-	}
-}
+$base=dirname($path);
+$free = @exec('if [ -d "'.$path.'" ]; then df --output=avail '.$path.' 2>/dev/null |grep -v Avail; fi');
+if ( $free == "" ) $free = @exec('if [ -d "'.$base.'" ]; then df --output=avail '.$base.' 2>/dev/null |grep -v Avail; fi');
+if ( $free == "" ) $free = "?";
 echo $free;
