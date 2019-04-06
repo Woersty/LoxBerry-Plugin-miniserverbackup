927549b7c142e7e2bba66557ec66148db82e3a71
Wörsty <git@loxberry.woerstenfeld.de>
2019-04-06 23:30:21 +0200

    Issue when showing free space
    Fixes #39

--- webfrontend/html/free.php
+++ webfrontend/html/free.php
@@ -4,7 +4,7 @@
 // Free space in workdir
 error_reporting(0);
 header('Content-Type: text/plain; charset=utf-8');
-$path=file_get_contents("/tmp/msb_free_space");
+$path=str_replace("\n", "", file_get_contents("/tmp/msb_free_space"));
 $base=dirname($path);
 $free = @exec('if [ -d "'.$path.'" ]; then df --output=avail '.$path.' 2>/dev/null |grep -v Avail; fi');
 if ( $free == "" ) $free = @exec('if [ -d "'.$base.'" ]; then df --output=avail '.$base.' 2>/dev/null |grep -v Avail; fi');
